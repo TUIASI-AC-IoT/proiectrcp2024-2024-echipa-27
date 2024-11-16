66c7ab761a2422f887fa07cdac0ac2a1cc969c34
MarcuCtin <constantin-marcu.ciobanu@student.tuiasi.ro>
2024-11-16 17:53:50 +0200

    implemented subscribe and improved connect-connack

--- mqtt_conn_packet.py
+++ mqtt_conn_packet.py
@@ -5,14 +5,6 @@ def create_connect_packet(client_id="test_client"):
     protocol_level = 0b00000101 
     connect_flags = 0b00000010  #doar clean start deocamdată
     keep_alive = 60  
-
-    #
-    # print(len(protocol_name).to_bytes(2, 'big')," protocol name",protocol_name.encode())
-    
-    # print(protocol_level.to_bytes(1, 'big'))
-    # print(connect_flags.to_bytes(1, 'big'))
-    # print(keep_alive.to_bytes(2, 'big'))
-
     variable_header = (
         len(protocol_name).to_bytes(2, 'big') +
         protocol_name.encode() +
@@ -27,14 +19,34 @@ def create_connect_packet(client_id="test_client"):
         client_id.encode()
     )
     print("payload",payload)
-    packet_type = 0x10 
+    fixed_header = 0x10 
     remaining_length = len(variable_header) + len(payload)
     print("remaining_length",remaining_length)
     connect_packet = (
+        fixed_header.to_bytes(1, 'big') +
+        remaining_length.to_bytes(1, 'big') +
+        variable_header +
+        payload
+        + b'\x05'+b'\x00\x00\x00\x00\x00'  # will, username, password
+    )
+    print("connect_packet",connect_packet)
+    return connect_packet
+
+
+def create_subscribe_packet(topic, qos=0, packet_id=1):
+    packet_type = 0x82  # 0x1000 0010
+    variable_header = packet_id.to_bytes(2, 'big')
+
+    topic_encoded = len(topic).to_bytes(2, 'big') + topic.encode() + qos.to_bytes(1, 'big')
+    payload = topic_encoded
+
+    remaining_length = len(variable_header) + len(payload)
+
+    subscribe_packet = (
         packet_type.to_bytes(1, 'big') +
         remaining_length.to_bytes(1, 'big') +
         variable_header +
         payload
     )
-    print("connect_packet",connect_packet)
-    return connect_packet
+    print("subscribe_packet",subscribe_packet)
+    return subscribe_packet
--- mqtt_connect_main.py
+++ mqtt_connect_main.py
@@ -1,38 +1,79 @@
-
 import socket
-from mqtt_conn_packet import create_connect_packet 
+from mqtt_conn_packet import create_connect_packet, create_publish_packet, create_subscribe_packet
 
-BROKER_HOST = "broker.hivemq.com"
+BROKER_HOST = 'broker.hivemq.com'
 BROKER_PORT = 1883
-print("Conectare la brokerul MQTT:", BROKER_HOST, "pe portul", BROKER_PORT)
+
 def connect_to_broker():
     try:
-            #initializare sockett
+        # Initializare socket
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((BROKER_HOST, BROKER_PORT))
-        
 
-        # Trimiterea pachetului
+        # Trimiterea pachetului CONNECT
         client_id = "test_client"
         client_socket.sendall(create_connect_packet(client_id))
         print("CONNECT trimis")
 
-        # primire connack
+        # Primire pachet CONNACK
         connack_packet = client_socket.recv(4)
-        print(connack_packet)
+        print("connack_packet:", connack_packet)
         packet_type = connack_packet[0] >> 4
         print("Tip pachet:", packet_type)
-        if packet_type == 2:  #if connack
-            print("connack primit")
-            print("success")
+        if packet_type == 2:  # Verificăm dacă este CONNACK
+            print("CONNACK primit")
+            print("Conectare reușită")
+            return client_socket
         else:
-            print("nu am primit connack")
-        client_socket.close()
-        print("exit")
-        exit(1)
+            print("Nu s-a primit un pachet CONNACK valid")
+            client_socket.close()
+            return None
     except Exception as e:
-        print("error", e)
-        exit(0)
+        print("Eroare la conectarea la broker:", e)
+        return None
+
+
+def subscribe_topic(broker_socket, topic):
+    try:
+        subscribe_packet = create_subscribe_packet(topic)
+        broker_socket.sendall(subscribe_packet)
+        print(f"Solicitare de abonare trimisă pentru topicul '{topic}'.")
+        suback_packet = broker_socket.recv(5)
+        print("suback_packet:", suback_packet)
+        if len(suback_packet) < 5:
+            print("Pachet SUBACK incomplet primit.")
+            return False
+        packet_type = suback_packet[0] >> 4
+        print("Tip pachet suback:", packet_type)
+        if packet_type != 9:
+            print("Pachet SUBACK invalid.")
+            return False
+        return_code = suback_packet[4]
+        print('return_code',return_code)
+        if return_code == 0x80:
+            print("abonarea a fost refuzată.")
+            return False
+        else:
+            print("Abonare reușită")
+            return True
+    except Exception as e:
+        print(f"Eroare la abonare: {e}")
+        return False
+
+
+def main():
+    host = BROKER_HOST
+    port = BROKER_PORT
+    client_id = 'test_client'
+
+    broker_socket = connect_to_broker()
+    if broker_socket is None:
+        return
+    subscribed = subscribe_topic(broker_socket, 'senzor/temperatura/living')
+    if not subscribed:
+        broker_socket.close()
+        return
+    broker_socket.close()
 
 if __name__ == "__main__":
-    connect_to_broker()
+    main()
